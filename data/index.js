02b9eff08371aa288011f73ea2dd34a6e34abe3d
Anouer Hermassi <ahermass@stevens.edu>
2020-04-07 15:49:39 -0400

    Implemented addProject

---
+++ data/index.js
@@ -0,0 +1,7 @@
+const userData = require("./users");
+const projectData = require("./projects");
+
+module.exports = {
+    users: userData,
+    projects: projectData
+};
---
+++ data/projects.js
@@ -0,0 +1,40 @@
+const mongoCollections = require('../config/mongoCollections');
+const data = require('../data');
+const projects = mongoCollections.projects;
+const users = mongoCollections.users;
+const userData = data.users;
+
+const { ObjectId } = require('mongodb');
+
+module.exports = {
+    async addProject(projectTitle, projectCategory, projectCreator, projectDate, projectPledgeGoal,
+                     projectCollected=0, projectBackers=[], projectDescription) {
+
+        if (!projectTitle) throw 'You must provide a title for your project';
+        if (!projectCategory) throw 'You must provide a category for your project';
+        if (!projectCreator) throw 'You must provide a creator for your project';
+        if (!projectDate) throw 'You must provide a date for your project';
+        if (!projectPledgeGoal) throw 'You must provide a pledge goal for your project';
+        if (!projectDescription) throw 'You must provide a description for your project';
+
+        const projectsCollection = await projects();
+
+        let newProject = {
+            title: projectTitle,
+            category: projectCategory,
+            creator: projectCreator,
+            date: projectDate,
+            pledgeGoal: projectPledgeGoal,
+            collected: projectCollected,
+            backers: projectBackers,
+            description: projectDescription
+        };
+
+        const insertInfo = await projectsCollection.insertOne(newProject);
+        if (insertInfo.insertedCount === 0) throw 'Could not add project';
+
+        const newId = insertInfo.insertedId;
+
+        return await this.getProject(newId);
+    }
+};
