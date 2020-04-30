4c8fe869bb771ba7ae3f41123eb50e4a9cb2c63a
Anouer Hermassi <ahermass@stevens.edu>
2020-04-30 13:05:44 -0400

    Comments client-side validation

---
+++ public/js/comments.js
@@ -0,0 +1,16 @@
+let form = document.getElementById('comment-form');
+let commentInput = document.getElementById('comment');
+let errorDiv = document.getElementById('empty-comment');
+
+form.addEventListener('submit', (event) => {
+   event.preventDefault();
+   if(!commentInput.value) {
+       errorDiv.hidden = false;
+       errorDiv.innerHTML = 'Comment can\'t be empty!';
+       errorDiv.focus();
+   }
+   else {
+       errorDiv.hidden = true;
+       form.submit();
+   }
+});
