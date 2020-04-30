db767ea964b1b9fa9aa94b4830da343045a70c2d
Anouer Hermassi <ahermass@stevens.edu>
2020-04-30 18:49:52 -0400

    Modified sign up form validation

--- public/js/register.js
+++ public/js/register.js
@@ -1,36 +1,46 @@
-const registerForm = document.getElementById("register-form")
-if (registerForm){
-    firstNameElement = document.getElementById("first_name");
-    lastNameElement = document.getElementById("last_name");
-    emailElement = document.getElementById("email");
-    passwordElement = document.getElementById("password");
-    confirmPasswordElement = document.getElementById("password_confirm");
-    cityElement = document.getElementById("city");
-    stateElement = document.getElementById("state");
-    errorDivRegister = document.getElementById("errordiv");
-    registerForm.addEventListener("submit", event => {
-        if(firstNameElement.value && lastNameElement.value && emailElement.value && passwordElement.value && confirmPasswordElement.value && cityElement.value && stateElement.value){
-            if(passwordElement.value == confirmPasswordElement.value){
+const registerForm = document.getElementById("register-form");
+
+const registerFirstName = document.getElementById("first_name");
+const registerLastName = document.getElementById("last_name");
+const registerEmail = document.getElementById("email");
+const registerPassword = document.getElementById("password");
+const registerConfirmPassword = document.getElementById("password_confirm");
+const registerCity = document.getElementById("city");
+const registerState = document.getElementById("state");
+
+const registerFirstNameError = document.getElementById("no-first-name");
+const registerLastNameError = document.getElementById("no-last-name");
+const registerEmailError = document.getElementById("no-email");
+const registerPasswordError = document.getElementById("no-password");
+const registerConfirmPasswordError = document.getElementById("no-password-confirmation");
+const registerPasswordsMismatch = document.getElementById('passwords-mismatch');
+const registerCityError = document.getElementById("no-city");
+const registerStateError = document.getElementById("no-state");
+
+if (registerForm) {
+
+    registerForm.addEventListener("submit", (event) => {
+        if(registerFirstName.value && registerLastName.value && registerEmail.value && registerPassword.value &&
+            registerConfirmPassword.value && registerCity.value && registerState.value) {
+            if(registerPassword.value === registerConfirmPassword.value)
                 registerForm.submit();
-            }
-            else{
+            else {
                 event.preventDefault();
-                errorDivRegister.hidden = false;
-                errorDivRegister.innerHTML = 'password and confirm password dont match!';
+                registerPasswordError.hidden = true;
+                registerConfirmPasswordError.hidden = true;
+                registerPasswordsMismatch.hidden = registerPassword.value === registerConfirmPassword.value;
             }
-
         }
-        //if(!firstNameElement.value || !lastNameElement.value || !emailElement.value || !passwordElement || !confirmPasswordElement.value || !cityElement.value ||  !stateElement.value){
-        else{
+        else {
             event.preventDefault();
-            errorDivRegister.hidden = false;
-            errorDivRegister.innerHTML = 'You Must Enter a valid firstname, lastname, email, password , city and state!';
-            //emailElement.focus()
-            //passwordElement.focus()
-
-        }  
-
-
+            registerFirstNameError.hidden = registerFirstName.value;
+            registerLastNameError.hidden = registerLastName.value;
+            registerEmailError.hidden = registerEmail.value;
+            registerPasswordError.hidden = registerPassword.value;
+            registerConfirmPasswordError.hidden = registerConfirmPassword.value;
+            registerCityError.hidden = registerCity.value;
+            registerStateError .hidden = registerState.value;
+        }
     })
 }
 
--- public/js/signin.js
+++ public/js/signin.js
@@ -1,21 +1,21 @@
 const signInForm = document.getElementById("signin-form");
-const emailElement = document.getElementById("email");
-const passwordElement = document.getElementById("password");
-const emailError = document.getElementById('no-email');
-const passwordError = document.getElementById('no-password');
+const loginEmail = document.getElementById("email");
+const loginPassword = document.getElementById("password");
+const loginEmailError = document.getElementById('no-email');
+const loginPasswordError = document.getElementById('no-password');
 
 if(signInForm) {
 
     signInForm.addEventListener("submit", (event) => {
         
-        if (emailElement.value && passwordElement.value) {
-            emailError.hidden = true;
-            passwordError.hidden = true;
+        if (loginEmail.value && loginPassword.value) {
+            loginEmailError.hidden = true;
+            loginPasswordError.hidden = true;
             signInForm.submit();
         } else {
             event.preventDefault();
-            emailError.hidden = emailElement.value;
-            passwordError.hidden = passwordElement.value;
+            loginEmailError.hidden = loginEmail.value;
+            loginPasswordError.hidden = loginPassword.value;
         }
     })
 }
